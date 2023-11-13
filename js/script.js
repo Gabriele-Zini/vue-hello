957c17175b9836d82e71cf45a1eae2f3238f1ebf
Gabriele-Zini <gabrielezini845@gmail.com>
2023-11-13 16:04:36 +0100

    fix: spell error

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 const app = createApp({
   data() {
     return {
-      message: "Hello Wolrd!",
+      message: "Hello World!",
       mySrc:
         "https://images.pexels.com/photos/18457182/pexels-photo-18457182/free-photo-of-animale-albero-giungla-fauna-selvatica.jpeg",
       myClass: "d-hidden",
