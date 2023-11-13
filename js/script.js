b42de459f04c7506175d5be13a9613cb65f8124f
Gabriele-Zini <gabrielezini845@gmail.com>
2023-11-13 14:24:02 +0100

    add: showImg and clearImg button and img elem

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const { createApp } = Vue;
+
+const app = createApp({
+  data() {
+    return {
+      message: "Hello Wolrd!",
+      mySrc:
+        "https://images.pexels.com/photos/18457182/pexels-photo-18457182/free-photo-of-animale-albero-giungla-fauna-selvatica.jpeg",
+      myClass: "d-hidden",
+    };
+  },
+
+  methods: {
+    showImg: function () {
+      this.myClass = "active";
+    },
+    clearImg: function () {
+      this.myClass = "d-hidden";
+    },
+  },
+});
+
+app.mount("#app");
